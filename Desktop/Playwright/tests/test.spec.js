596d41bf6bc354f9e2248eea64e14a2a5e6140c8
Yuliya.S <yuliyamarketplace@yandex.ru>
2025-03-10 21:06:21 +0400

    find the error

--- Desktop/Playwright/tests/test.spec.js
+++ Desktop/Playwright/tests/test.spec.js
@@ -4,19 +4,25 @@ const { email, password } = require("../user");
 
 import { test, expect } from "@playwright/test";
 
+
 test("test", async ({ page }) => {
   await page.goto("https://netology.ru/");
   await page.getByRole("link", { name: "Войти" }).click();
   await page.getByRole("textbox", { name: "Email" }).click();
-  await page
-    .getByRole("textbox", { name: "Email" })
-    .fill("sunjuly82@yandex.ru");
+  await page.getByRole("textbox", { name: "Email" }).fill("sunjuly82@yandex.ru");
   await page.getByRole("textbox", { name: "Пароль" }).click();
   await page.getByRole("textbox", { name: "Пароль" }).fill("18102017");
   await page.getByTestId("login-submit-btn").click();
+
+  await expect (page.locator("popmechanic-desktop popmechanic-landscape popmechanic-loaded").toContainText("Инженер по тестированию:расширенный курс"));
+
 });
 
+/*
+
 import { test, expect } from "@playwright/test";
+
+
 
 test("test", async ({ page }) => {
   await page.goto("https://netology.ru/");
@@ -31,3 +37,5 @@ test("test", async ({ page }) => {
 
   await expect(page.locator([userId, 7872227]));
 });
+
+*/
