8e075127222c30c72ad9a5f02fbe87cf04933b80
Yuliya.S <yuliyamarketplace@yandex.ru>
2025-03-11 19:28:12 +0400

    find the error

---
+++ Desktop/Playwright/e2e/test.spec.js
@@ -0,0 +1,41 @@
+const { chromium } = require("playwright");
+const { test, expect } = require("playwright");
+const { email, password } = require("../user");
+
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await page.getByRole("textbox", { name: "Email" }).click();
+  await page
+    .getByRole("textbox", { name: "Email" })
+    .fill("sunjuly82@yandex.ru");
+  await page.getByRole("textbox", { name: "Пароль" }).click();
+  await page.getByRole("textbox", { name: "Пароль" }).fill("18102017");
+  await page.getByTestId("login-submit-btn").click();
+
+  const heading = page.locator("//h2");
+
+  await expect.heading.toContainText("Моё обучение");
+});
+
+import { test, expect } from "@playwright/test";
+
+test("test", async ({ page }) => {
+  await page.goto("https://netology.ru/");
+  await page.getByRole("link", { name: "Войти" }).click();
+  await page.getByRole("textbox", { name: "Email" }).click();
+  await page
+    .getByRole("textbox", { name: "Email" })
+    .fill("sunjuly82@yandex.ru");
+  await page.getByRole("textbox", { name: "Пароль" }).click();
+  await page.getByRole("textbox", { name: "Пароль" }).fill("12345678");
+  await page.getByTestId("login-submit-btn").click();
+
+  //await expect(page.locator([userId, 7872227]));
+
+  const heading = page.locator("");
+
+  await expect.heading.toContainText("Вы ввели неправильно логин или пароль");
+});
--- Desktop/Playwright/tests/test.spec.js
+++ Desktop/Playwright/tests/test.spec.js
@@ -1,22 +1,24 @@
-const { chromium } = require("playwright");
-const { test, expect } = require("playwright");
-const { email, password } = require("../user");
+//const { chromium } = require("playwright");
+//const { test, expect } = require("playwright");
+//const { email, password } = require("../user");
 
-import { test, expect } from "@playwright/test";
+//import { test, expect } from "@playwright/test";
 
 
-test("test", async ({ page }) => {
-  await page.goto("https://netology.ru/");
-  await page.getByRole("link", { name: "Войти" }).click();
-  await page.getByRole("textbox", { name: "Email" }).click();
-  await page.getByRole("textbox", { name: "Email" }).fill("sunjuly82@yandex.ru");
-  await page.getByRole("textbox", { name: "Пароль" }).click();
-  await page.getByRole("textbox", { name: "Пароль" }).fill("18102017");
-  await page.getByTestId("login-submit-btn").click();
+//test("test", async ({ page }) => {
+  //await page.goto("https://netology.ru/");
+  //await page.getByRole("link", { name: "Войти" }).click();
+  //await page.getByRole("textbox", { name: "Email" }).click();
+  //await page.getByRole("textbox", { name: "Email" }).fill("sunjuly82@yandex.ru");
+  //await page.getByRole("textbox", { name: "Пароль" }).click();
+  //await page.getByRole("textbox", { name: "Пароль" }).fill("18102017");
+  //await page.getByTestId("login-submit-btn").click();
 
-  await expect (page.locator("popmechanic-desktop popmechanic-landscape popmechanic-loaded").toContainText("Инженер по тестированию:расширенный курс"));
+  //const heading = page.locator('//h2'); 
 
-});
+  //await expect.heading.toContainText('Моё обучение');
+
+  //await expect (page.locator("popmechanic-desktop popmechanic-landscape popmechanic-loaded").toContainText("Инженер по тестированию:расширенный курс"));});
 
 /*
 
